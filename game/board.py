8718d1736bb233d0378375312489ad24943a25e4
jachym <jachym.lowenhoffer@gmail.com>
2026-05-10 22:17:48 +0200

    checking the end of the game

--- game/board.py
+++ game/board.py
@@ -9,13 +9,22 @@ class Board:
         self.player = 1
         self.dirs = [(1,0),(0,1),(-1,0),(0,-1)]
 
+        self.white_stones = []
+        self.black_stones = []
+
+        self.passed = False
+        self.poss_moves_white = []
+        self.poss_moves_black = []
+
     def setup_board(self):
         self.board[3][3]=1
         self.board[4][4]=1
+        self.white_stones.append((3,3),(4,4))
 
         self.board[3][4]=-1
         self.board[4][3]=-1
-        logger.info(f'{self.board}')
+        self.black_stones.append((3,4),(4,3))
+        logger.info(f'{self.board}',f'{self.white_stones,self.black_stones}')
 
     def _find_anchor(self,tile:tuple[int])->list:
         """Finds the nearest same-colored piece to which the whole path is
@@ -58,7 +67,7 @@ class Board:
         for difference in anchors:
             distance = abs(difference[0]+difference[1])
             direction = tuple(np.array(difference)/distance)
-            logger.info(f'difference:{difference},distanece:{distance},direction:{direction}')
+            logger.info(f'difference:{difference},distanece:{distance},direction:{direction}') 
 
             for i in range(1,distance):
                 vzdalenost = tuple(int(x)*i for x in direction) 
@@ -69,6 +78,44 @@ class Board:
 
                 if self.board[looking_at] == 0:
                     logger.error('There should always be stone between tile and anchor')
-                           
+
+    def get_possible_moves(self):
+        ...
+    def _is_viable(self,tile:tuple):
+        ...
+    #somethig to handle the passing, preferably in this class so it is all
+    # self-ontained.                        
+    def is_game_over(self):
+        """Determins if the game is over because one player is out of stones or
+        that both players passed"""
+
+        if not self._stones_left():
+            return True
+        if self.player==1:
+            opposite_poss_moves = self.poss_moves_black
+        else:
+            opposite_poss_moves = self.poss_moves_white
+        if self.passed and not opposite_poss_moves:
+            return True
+        return False
+
+    def determine_winner(self):
+        """Checks the number of stones left and return the number of winning
+        player"""
+
+        white_won = len(self.white_stones)>len(self.black_stones)
+        if white_won:
+            return 1
+        else:
+            return -1
+
+    def _stones_left(self):
+        """Checks if any player is out of stones"""
+
+        if not self.white_stones:
+            return False
+        if not self.black_stones:
+            return False
+        return True
 
 
