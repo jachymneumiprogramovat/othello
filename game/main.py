0d50850cf964b5f7b03989b18eeb5ef054dd8103
jachym <jachym.lowenhoffer@gmail.com>
2026-06-29 10:07:22 +0200

    made the code and logs more readible hopefuly

--- game/main.py
+++ game/main.py
@@ -12,36 +12,34 @@ from board import Board
 def main():
     """Main function"""
 
+    # Initing pygame
     os.environ['SDL_VIDEO_WINDOW_POS'] = "1100,200"
     pg.mixer.pre_init(44100, -16, 2, 2048)
-    # Initing pygame
     pg.init()
     screen = pg.display.set_mode((WIDTH, HEIGHT),pg.SRCALPHA)
     pg.display.set_caption(CAPTION)
-    logger.info('Pygame setup')
+    logger.success('Pygame setup')
 
     clock = pg.time.Clock()
     running = True
 
-    
     game = Game(screen)
     game.setup_screen()
     pg.display.flip()
-    logger.info('screen setup')
+    logger.success('screen setup')
 
     board = Board()
     board.setup_board()
-    logger.info('logic setup')
-
+    logger.success('logic setup')
     logger.info(f'{board.board}')
-
 
     poss_moves = board.get_possible_moves()
     pg.display.update(game.draw_squares(poss_moves,HIGHLITE_COLOR))
     logger.info(f'poss_moves')
+
     while running:
         clock.tick(TIKS)
-        rect_to_change = []
+        rects_to_change = []
 
         for event in pg.event.get():
             # Check quit event
@@ -51,20 +49,25 @@ def main():
             # Check mouse click
             if event.type == pg.MOUSEBUTTONDOWN:
                 logger.debug("Mouse click signal received.")
-                tile = tuple(np.array(event.pos)//100)
-                tile = (tile[1],tile[0])
-                logger.info(str(tuple(int(x) for x in tile)))
+                tile_width = WIDTH//100
+                tile_height = HEIGHT//100
+
+                # process the tile click
+                x, y = event.pos
+                tile = (y // tile_height, x // tile_width)
+                logger.info(f'{tile}')
                 
                 poss_moves = board.poss_moves[board.player]
                 logger.info(f'{tile},{[x for x in poss_moves]}')
                 
                 if not tile in poss_moves:
+                    logger.error(f'bro doslova mas vyznacene ty kam muzes :sob:')
                     continue
 
                 # play the move 
-                to_change = board.play_move(tile)
+                tile_to_change = board.play_move(tile)
                 curr_color = WHITE_COLOR if board.player ==-1 else BLACK_COLOR
-                rect_to_change=game.draw_squares(to_change,curr_color)
+                rects_to_change=game.draw_squares(tile_to_change,curr_color)
 
                 #check for end of the game
                 if board.is_game_over():
@@ -77,11 +80,11 @@ def main():
                 next_poss_moves = board.get_possible_moves()
 
                 poss_moves.remove(tile)
-                rect_to_change+=game.draw_squares(poss_moves, TILE_COLOR)
-                rect_to_change+=game.draw_squares(next_poss_moves,HIGHLITE_COLOR)
+                rects_to_change+=game.draw_squares(poss_moves, TILE_COLOR)
+                rects_to_change+=game.draw_squares(next_poss_moves,HIGHLITE_COLOR)
 
-                logger.info(rect_to_change)
-        pg.display.update(rect_to_change)
+                logger.info(rects_to_change)
+        pg.display.update(rects_to_change)
 
 
 
@@ -90,17 +93,45 @@ def main():
 if __name__ == "__main__":
     base_dir = pathlib.Path(__file__).parent.resolve()
     logger.remove()
+    info_filter = lambda record: record["level"].name == "INFO"
+    success_filter = lambda record: record["level"].name == "SUCCESS"
+    error_filter = lambda record: record["level"].name == "ERROR"
+
     logger.add(
         os.path.join(base_dir, "main.log"),
         colorize=True,
         format="<green>{file}/{function}/{line}</green> <level>{message}</level>",
-        level="INFO",
+        filter=info_filter,
     )
     logger.add(
         sys.stdout,
         colorize=True,
         format="<green>{file}/{function}/{line}</green> <level>{message}</level>",
-        level="INFO",
+        filter=info_filter,
+    )
+    logger.add(
+        os.path.join(base_dir, "main.log"),
+        colorize=True,
+        format="<yellow>{file} {message} </yellow>",
+        filter=success_filter
+    )
+    logger.add(
+        sys.stdout, 
+        colorize=True,
+        format="<yellow>{file} {message} </yellow>",
+        filter=success_filter
+    )
+    logger.add(
+        os.path.join(base_dir, "main.log"),
+        colorize=True,
+        format="<red>{file}/{function}/{line} {message}</red>",
+        filter=error_filter,
+    )
+    logger.add(
+        sys.stdout,
+        colorize=True,
+        format="<red>{file}/{function}/{line} {message}</red>",
+        filter=error_filter,
     )
     main()
 
