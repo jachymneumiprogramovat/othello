49e0e50a1b90b414e6541fc5602f97d3f61a276a
jachym <jachym.lowenhoffer@gmail.com>
2026-07-05 16:24:59 +0200

    hra najednou rychla?

--- game/automate.py
+++ game/automate.py
@@ -1,12 +1,17 @@
-from board import Board
+from game.board import Board
 import numpy as np
 from random import randint
 import matplotlib.pyplot as plt
 import time
-
+from loguru import logger
+from tqdm import tqdm
 """
 Othello played automatically and randomly to gather inside of the game
 """
+
+logger.remove()
+
+ITERATIONS = 50000
 
 board = Board()
 board.setup_board()
@@ -17,30 +22,31 @@ board.get_possible_moves()
 white_stones = []
 
 start = time.time()
-while not board.is_game_over():
-    poss_moves = board.get_possible_moves()
-    print(poss_moves, board.poss_moves)
-    
-    number_poss_moves.append(len(poss_moves))
-    white_stones.append(board.stones[-1])
-    if not poss_moves:
-        board.player *=-1
-        continue
-    number_of_moves+=1
+for i in tqdm(range(ITERATIONS)):
+    while not board.is_game_over():
+        poss_moves = board.get_possible_moves()
 
-    move = poss_moves[randint(0,len(poss_moves)-1)]
-    board.play_move(move)
-    board.player *=-1
+        number_poss_moves.append(len(poss_moves))
+        white_stones.append(board.stones[-1])
+        if not poss_moves:
+            board.player *=-1
+            continue
+        number_of_moves+=1
+
+        move = poss_moves[randint(0,len(poss_moves)-1)]
+        board.board, _ = board.play_move(move)
+        board.player *=-1
+    board.setup_board()
 end=time.time()
-print(board.determine_winner())
+# print(board.determine_winner())
 #print(number_poss_moves)
 #print(number_of_moves)
 print(end-start)
 white_poss_moves = number_poss_moves[::2]
 #print(white_poss_moves)
-plt.plot(range(len(white_poss_moves)),white_poss_moves,color='blue')
-print(white_stones)
-plt.plot(range(len(white_stones[::2])),white_stones[::2],color='red')
+#plt.plot(range(len(white_poss_moves)),white_poss_moves,color='blue')
+#print(white_stones)
+#plt.plot(range(len(white_stones[::2])),white_stones[::2],color='red')
 
 #plt.plot(range(len(number_poss_moves)),number_poss_moves)
-plt.show()
+#plt.show()
